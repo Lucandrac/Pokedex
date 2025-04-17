9e1e52271f8a394ce00e89e25727b4f506d2a4fc
Lucandrac <lucandrac@gmail.com>
2025-04-17 13:48:51 +0200

    changement classe

--- Pokedex/Model/JsonResponse.swift
+++ Pokedex/Model/JsonResponse.swift
@@ -1,10 +1,3 @@
-//
-//  JsonResponse.swift
-//  Pokedex
-//
-//  Created by Stagiaire on 17/04/2025.
-//
-
 import Foundation
 
 struct Pokemon: Decodable {
@@ -13,18 +6,38 @@ struct Pokemon: Decodable {
     let category: String
     let name: Name
     let sprites: Sprites
-    let types: [String]?
-    let talents: [String]?
-    let stats: [String: Int]?
-    let resistances: [String]?
-    let evolution: [String]?
-    let height: Double?
-    let weight: Double?
-    let eggGroups: [String]?
-    let sexe: String?
-    let catchRate: Int?
-    let level100: [String: Int]?
+    let types: [TypeElement]
+    let talents: [Talent]
+    let stats: Stats
+    let resistances: [Resistance]
+    let evolution: Evolution
+    let height: String
+    let weight: String
+    let eggGroups: [String]
+    let sexe: Sexe
+    let catchRate: Int
+    let level100: Int
     let formes: [String]?
+
+    enum CodingKeys: String, CodingKey {
+        case pokedexID = "pokedex_id"
+        case generation
+        case category
+        case name
+        case sprites
+        case types
+        case talents
+        case stats
+        case resistances
+        case evolution
+        case height
+        case weight
+        case eggGroups = "egg_groups"
+        case sexe
+        case catchRate = "catch_rate"
+        case level100 = "level_100"
+        case formes
+    }
 }
 
 struct Name: Decodable {
@@ -34,8 +47,63 @@ struct Name: Decodable {
 }
 
 struct Sprites: Decodable {
-    let regular: String?
-    let shiny: String?
+    let regular: String
+    let shiny: String
     let gmax: String?
 }
 
+struct TypeElement: Decodable {
+    let name: String
+    let image: String
+}
+
+struct Talent: Decodable {
+    let name: String
+    let tc: Bool
+}
+
+struct Stats: Decodable {
+    let hp: Int
+    let atk: Int
+    let def: Int
+    let speAtk: Int
+    let speDef: Int
+    let vit: Int
+
+    enum CodingKeys: String, CodingKey {
+        case hp
+        case atk
+        case def
+        case speAtk = "spe_atk"
+        case speDef = "spe_def"
+        case vit
+    }
+}
+
+struct Resistance: Decodable {
+    let name: String
+    let multiplier: Double
+}
+
+struct Evolution: Decodable {
+    let pre: EvolutionElement?
+    let next: [EvolutionElement]
+    let mega: EvolutionElement?
+}
+
+struct EvolutionElement: Decodable {
+    let pokedexID: Int?
+    let name: String?
+    let condition: String?
+
+    enum CodingKeys: String, CodingKey {
+        case pokedexID = "pokedex_id"
+        case name
+        case condition
+    }
+}
+
+struct Sexe: Decodable {
+    let male: Double
+    let female: Double
+}
